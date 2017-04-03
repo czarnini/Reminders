751f818fbbd55114a2a4dc4b3d1e1e8dbf685c40
czarnini <mboguck2@elka.pw.edu.pl>
2017-04-03 11:37:02 +0200

    listViewCreated

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -2,12 +2,19 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class RemindersActivity extends AppCompatActivity {
-
+public class RemindersActivity extends AppCompatActivity
+{
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.reminders_row, R.id.row_text, new String[]{"1","2","312342352323234234242"});
     }
 }
