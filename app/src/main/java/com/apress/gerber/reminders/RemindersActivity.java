6d12120a15680713511cc4fd273e6d4faa590946
czarnini <mboguck2@elka.pw.edu.pl>
2017-04-08 11:41:12 +0200

    Added options menu

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,7 +1,10 @@
 package com.apress.gerber.reminders;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,5 +19,29 @@ public class RemindersActivity extends AppCompatActivity
         mListView = (ListView) findViewById(R.id.reminders_list_view);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.reminders_row, R.id.row_text, new String[]{"1","2","312342352323234234242"});
+        mListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reminders, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch ( item.getItemId())
+        {
+            case R.id.action_new:
+                Log.d(getLocalClassName(), "New reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
+
+        }
     }
 }
