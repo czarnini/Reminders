d5ea51cdbceba0fc84e34f7e639c7d7b1df8b607
czarnini <mboguck2@elka.pw.edu.pl>
2017-04-08 21:22:35 +0200

    Dialog box for Edit and delete Reminder

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,21 +1,29 @@
 package com.apress.gerber.reminders;
 
+import android.app.Dialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class RemindersActivity extends AppCompatActivity {
+public class RemindersActivity extends AppCompatActivity
+{
     private ListView mListView;
     private RemindersDbAdapter mDbAdapter;
     private SimpleCursorAdapter mCursorAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
 
@@ -30,11 +38,48 @@ public class RemindersActivity extends AppCompatActivity {
         int[] to = {R.id.row_text};
         mCursorAdapter = new RemindersSimpleCursorAdapter(this, R.layout.reminders_row, cursor, from, to, 0);
         mListView.setAdapter(mCursorAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int masterListPosition, long l)
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RemindersActivity.this);
+                ListView modeListView = new ListView(RemindersActivity.this);
+                String[] modes = {"Edytuj", "Usuń"};
+                ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(RemindersActivity.this,
+                        android.R.layout.simple_expandable_list_item_1, android.R.id.text1, modes);
+                modeListView.setAdapter(modeAdapter);
+                builder.setView(modeListView);
+                final Dialog dialog = builder.create();
+                dialog.show();
+                modeListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+                    {
+                        if (i == 0)
+                        {
+                            Toast.makeText(RemindersActivity.this,
+                                    "Edycja pozycji " + masterListPosition, Toast.LENGTH_SHORT ).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(RemindersActivity.this,
+                                       "Usuwanie pozycji " + masterListPosition, Toast.LENGTH_SHORT ).show();
 
+                        }
+
+                        dialog.dismiss();
+
+                    }
+                });
+            }
+        });
 
     }
 
-    private void insertDummyData() {
+    private void insertDummyData()
+    {
         mDbAdapter.deleteAllReminders();
         mDbAdapter.createReminder("Spotkanie z Tomkiem", true);
         mDbAdapter.createReminder("Siłownia", false);
@@ -55,15 +100,18 @@ public class RemindersActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_reminders, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
             case R.id.action_new:
                 Log.d(getLocalClassName(), "New reminder");
                 return true;
